80b0b19c0db30dc5b7ac58d210fae69a4bc02bf8
sergius2oo9 <sergius2oo9@ya.ru>
2025-04-02 09:43:42 +0300

    rt-s1010

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,4 @@
-PADAVAN_REPO="https://gitlab.com/hadzhioglu/padavan-ng.git"
-PADAVAN_BRANCH="master"
-PADAVAN_COMMIT="HEAD"
-PADAVAN_TOOLCHAIN_URL="https://gitlab.com/api/v4/projects/hadzhioglu%2Fpadavan-ng/packages/generic/toolchain/latest/toolchain.tzst"
+#!/bin/sh
 
 sed -i 's/^CONFIG_RAETH_ESW_PORT_WAN=0/CONFIG_RAETH_ESW_PORT_WAN=4/' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
 sed -i 's/^CONFIG_RAETH_ESW_PORT_WAN=1/CONFIG_RAETH_ESW_PORT_WAN=3/' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
