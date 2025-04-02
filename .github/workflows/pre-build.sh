f533c264518b56ce36a488ee27b92946fd85194c
sergius2oo9 <sergius2oo9@ya.ru>
2025-04-02 12:54:46 +0300

    tl_c5-v4

--- .github/workflows/pre-build.sh
+++ .github/workflows/pre-build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sed 's/CONFIG_RT2880_FLASH_8M=y/CONFIG_RT2880_FLASH_16M=y/' padavan-ng/trunk/configs/boards/TPLINK/TL_C5-V4/kernel-3.4.x.config
+sed 's/CONFIG_RT_FIRST_IF_RF_OFFSET=0x7f0000/CONFIG_RT_FIRST_IF_RF_OFFSET=0x40000/' padavan-ng/trunk/configs/boards/TPLINK/TL_C5-V4/kernel-3.4.x.config
+sed 's/pt_tplink_8m.config/pt_tplink_16m.config/' padavan-ng/trunk/configs/boards/TPLINK/TL_C5-V4/partitions.config
