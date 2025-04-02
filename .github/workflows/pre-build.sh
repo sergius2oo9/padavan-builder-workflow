42960aebe8d2983161f7373bcfbfc9680ab5c04c
sergius2oo9 <sergius2oo9@ya.ru>
2025-04-02 11:52:52 +0300

    rt-s1010

--- .github/workflows/pre-build.sh
+++ .github/workflows/pre-build.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-sed -i 's/^CONFIG_RAETH_ESW_PORT_WAN=0/CONFIG_RAETH_ESW_PORT_WAN=4/' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
-sed -i 's/^CONFIG_RAETH_ESW_PORT_WAN=1/CONFIG_RAETH_ESW_PORT_WAN=3/' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
-sed -i 's/^CONFIG_RAETH_ESW_PORT_WAN=4/CONFIG_RAETH_ESW_PORT_WAN=1/' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
-sed -i 's/^CONFIG_RAETH_ESW_PORT_WAN=3/CONFIG_RAETH_ESW_PORT_WAN=0/' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
+sed -e 's/CONFIG_RAETH_ESW_PORT_WAN=0/CONFIG_RAETH_ESW_PORT_WAN=4/; s/CONFIG_RAETH_ESW_PORT_WAN=1/CONFIG_RAETH_ESW_PORT_WAN=3/; s/CONFIG_RAETH_ESW_PORT_WAN=4/CONFIG_RAETH_ESW_PORT_WAN=1/; s/CONFIG_RAETH_ESW_PORT_WAN=3/CONFIG_RAETH_ESW_PORT_WAN=0/' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
