6af91a5b008bf7e4aba95a1076e805c1f1000414
sergius2oo9 <sergius2oo9@ya.ru>
2025-04-13 02:27:46 +0300

    rt-s1010

--- .github/workflows/pre-build.sh
+++ .github/workflows/pre-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+sed -i 's/^#define BOARD_NUM_ETH_EPHY	3/#define BOARD_NUM_ETH_EPHY	5/g' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/board.h
 sed -i 's/^CONFIG_RAETH_ESW_PORT_WAN=0/CONFIG_RAETH_ESW_PORT_WAN=4/g' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
 sed -i 's/^CONFIG_RAETH_ESW_PORT_LAN1=1/CONFIG_RAETH_ESW_PORT_LAN1=3/g' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
 sed -i 's/^CONFIG_RAETH_ESW_PORT_LAN3=4/CONFIG_RAETH_ESW_PORT_LAN3=1/g' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
 sed -i 's/^CONFIG_RAETH_ESW_PORT_LAN4=3/CONFIG_RAETH_ESW_PORT_LAN4=0/g' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/kernel-3.4.x.config
-sed -i 's/^#define BOARD_NUM_ETH_EPHY	3/#define BOARD_NUM_ETH_EPHY	5/g' padavan-ng/trunk/configs/boards/SERCOMM/RT-S1010/board.h
