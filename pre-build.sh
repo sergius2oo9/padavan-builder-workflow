a60ac0bf5791dd81c9f48de5e7f47b52483cf6a0
sergius2oo9 <sergius2oo9@ya.ru>
2025-08-17 14:02:13 +0300

    tl_c50-v4 (mercusys-ac12)

--- pre-build.sh
+++ pre-build.sh
@@ -2,5 +2,5 @@
 
 sed -i 's/^CONFIG_RAETH_ESW_PORT_WAN=0/CONFIG_RAETH_ESW_PORT_WAN=4/' padavan-ng/trunk/configs/boards/TPLINK/TL_C50-V4/kernel-3.4.x.config
 sed -i 's/^CONFIG_RAETH_ESW_PORT_LAN1=1/CONFIG_RAETH_ESW_PORT_LAN1=3/' padavan-ng/trunk/configs/boards/TPLINK/TL_C50-V4/kernel-3.4.x.config
-sed -i 's/^CONFIG_RAETH_ESW_PORT_LAN3=4/CONFIG_RAETH_ESW_PORT_LAN3=1/' padavan-ng/trunk/configs/boards/TPLINK/TL_C50-V4/kernel-3.4.x.config
-sed -i 's/^CONFIG_RAETH_ESW_PORT_LAN4=3/CONFIG_RAETH_ESW_PORT_LAN4=0/' padavan-ng/trunk/configs/boards/TPLINK/TL_C50-V4/kernel-3.4.x.config
+sed -i 's/^CONFIG_RAETH_ESW_PORT_LAN3=3/CONFIG_RAETH_ESW_PORT_LAN3=1/' padavan-ng/trunk/configs/boards/TPLINK/TL_C50-V4/kernel-3.4.x.config
+sed -i 's/^CONFIG_RAETH_ESW_PORT_LAN4=4/CONFIG_RAETH_ESW_PORT_LAN4=0/' padavan-ng/trunk/configs/boards/TPLINK/TL_C50-V4/kernel-3.4.x.config
